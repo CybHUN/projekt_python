c85fd6f1a45319aa6141897c5c273549d4de0a25
CybHUN <nagyferi85@gmail.com>
2016-03-18 23:03:59 +0100

    Programok
    Pár alapprogram amiknek a forráskódja segíthet megérteni a python
    alapjait

--- ekf_projektlab.py
+++ ekf_projektlab.py
@@ -1,4 +1,5 @@
-#Hello World program
-
+#Hello World program -egyszerü szöveg kiírtás, minden programozási nyelv első programja
 print("Hello World")
-
+#Köszönő program - egy változót bekérünk és vele dolgozunk
+nev = input("Add meg a neved:")
+print("Udvozollek,"+nev)
