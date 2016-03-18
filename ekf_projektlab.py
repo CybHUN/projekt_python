26421d791c88f98ff3ae2ed906aa463b6fc10065
somieri <somieri123@gmail.com>
2016-03-18 23:47:36 +0100

    Added files via upload

--- ekf_projektlab.py
+++ ekf_projektlab.py
@@ -3,3 +3,7 @@ print("Hello World")
 #Köszönő program - egy változót bekérünk és vele dolgozunk
 nev = input("Add meg a neved:")
 print("Udvozollek,"+nev)
+
+a=input("a oldal hossza:")
+b=input("b oldal hossza:")
+print(a,b)
